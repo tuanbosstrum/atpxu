9f96e12edeb9ea73a9c0cb2814c811eb71a79e2f
tuan <tuanbosstrum@gmail.com>
2021-03-27 14:39:30 +0700

    AT

---
+++ AT/src/hdtmain.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MyPC
+ */
+public class hdtmain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Chào các bạn lớp Java SWING MVC");
+    }
+    
+}
